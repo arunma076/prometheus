9b93df77d26e001e4043a8dd6f5d0c9312acf415
arunma076 <arunma076@gmail.com>
2024-03-19 14:23:44 +0530

    version change

--- node exporter/install-node-exporter.sh
+++ node exporter/install-node-exporter.sh
@@ -3,9 +3,9 @@
 sudo useradd --no-create-home node_exporter
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
-tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
-sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_exporter
-rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
+tar xzf node_exporter-1.7.0.linux-amd64.tar.gz
+sudo cp node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.7.0.linux-amd64.tar.gz 
 
 sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
 
