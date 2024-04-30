663915b7ec10df104f026dc9ddb4a55115927662
arunma076 <arunma076@gmail.com>
2024-04-30 16:11:05 +0530

    new update

--- node exporter/install-node-exporter.sh
+++ node exporter/install-node-exporter.sh
@@ -7,6 +7,11 @@ sudo tar xzf node_exporter-1.7.0.linux-amd64.tar.gz
 sudo cp node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/node_exporter
 sudo rm -rf node_exporter-1.7.0.linux-amd64.tar.gz 
 
+sudo mkdir /etc/node_exporter
+sudo mv node-expo.beinex.pem privatekey.key config.yml /etc/node_exporter/
+sudo chown -R node_exporter:node_exporter /etc/node_exporter
+
+
 sudo cp node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
